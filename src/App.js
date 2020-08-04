51f2d021d6451caafdbc2476193098ecf10fb614
wildenali <wildenwildenaliali@gmail.com>
2020-08-04 23:36:53 +0700

    tugas 11, membuat tabel daftar buah

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import './App.css';
+import DaftarBuah from './tugas11/DaftarBuah';
+
+function App() {
+  return(
+    <div>
+      <DaftarBuah />
+    </div>
+  )
+}
+
+export default App;
 
 /*
 // Latihan props
@@ -61,4 +72,3 @@ function App() {
 
 export default App;
 */
-
