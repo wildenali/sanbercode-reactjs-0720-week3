fc0d477b90f409ea1764cd31b3fc30d90523c1b2
wildenali <wildenwildenaliali@gmail.com>
2020-08-04 22:06:41 +0700

    Cara membuat Komponen sederhana

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
-
 import './App.css';
+
+// Cara 1
+const Halow = () => {
+  return(
+    <>
+      <h1>Hayyyy</h1>
+    </>
+  )
+}
+
+// Cara 2
+class HaloKeles extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Ini HaloKeles</h1>
+      </>
+    )
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
       <h1>Cek</h1>
+      <Halow />
+      <HaloKeles />
     </div>
   );
 }
