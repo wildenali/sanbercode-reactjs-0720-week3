4d874c80f60cb00b8d64858a659c0cc59f511cc7
wildenali <wildenwildenaliali@gmail.com>
2020-08-04 22:15:14 +0700

    Cara mengghunakan Props untuk komponen function dan komponen class

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 
 // Cara 1
-const Halow = () => {
+const Halow = (props) => {
   return(
     <>
-      <h1>Hayyyy</h1>
+      <h1>Hayyyy {props.namaSiapa}</h1>
     </>
   )
 }
@@ -15,7 +15,17 @@ class HaloKeles extends React.Component {
   render() {
     return (
       <>
-        <h1>Ini HaloKeles</h1>
+        <h1>Ini HaloKeles {this.props.namaBebas}</h1>
+      </>
+    )
+  }
+}
+
+class HaloNama extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Ini Nama Lengkapnya {this.props.namaLengkap}</h1>
       </>
     )
   }
@@ -23,11 +33,20 @@ class HaloKeles extends React.Component {
 
 
 function App() {
+  var nama="Wilden"
+  var namaLengkap = ["Wilden", "Ali"]
   return (
     <div className="App">
       <h1>Cek</h1>
-      <Halow />
-      <HaloKeles />
+      <Halow namaSiapa={nama}/>
+      <HaloKeles namaBebas={nama}/>
+      {
+        namaLengkap.map(x => {
+          return(
+            <HaloNama namaLengkap={x} />
+          )
+        })
+      }
     </div>
   );
 }
