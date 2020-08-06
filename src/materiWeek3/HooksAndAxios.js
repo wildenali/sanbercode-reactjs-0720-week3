22070b1bec215f63055dbdfc236d0747bc812393
wildenali <wildenwildenaliali@gmail.com>
2020-08-06 14:27:13 +0700

    Membuat click counter dengan component Hooks

--- src/materiWeek3/HooksAndAxios.js
+++ src/materiWeek3/HooksAndAxios.js
@@ -1,4 +1,5 @@
 // Contoh Componen Class
+/*
 import React, { Component } from 'react'
 
 class HooksAndAxios extends Component {
@@ -19,5 +20,21 @@ class HooksAndAxios extends Component {
   }
 
 }
+export default HooksAndAxios
+*/
 
+// Contoh Component dengan Hooks
+import React, { useState } from 'react'
+
+const HooksAndAxios = () => {
+  // Deklarasi variable state baru ygn kita sebut "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>ANda menekan sebanyak {count} kali</p>
+      <button onClick={() => setCount(count + 1)}>Klik Saya</button>
+    </div>
+  );
+}
 export default HooksAndAxios
